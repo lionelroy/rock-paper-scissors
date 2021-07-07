e216a54e93880e46e96f6dd4e6fd3b661523a05e
Stephane Roy <stephroy411@hotmail.com>
2021-07-07 09:16:57 -0300

    Added text-shadow in header and changed colors.... Increased points from 5 to 10 to win the game

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,22 @@
-// To do: Logic for Rock Paper Scissors game
+
+let 
+
+function computerPlay() {
+    // random return (rock || paper || scissors)
+}
+
+function playRound(playerSelection, computerSelection) {
+    if(playerSelection = winner)
+    return ("You win!");
+    else if(computerSelection = winner)
+    return ("you Lose!")
+    else
+    return ("It's a draw!")
+}
+
+function game() {
+    // Loop 10 times
+    function playRound() {
+        // Keep score, reports loser or winner
+    }
+}
