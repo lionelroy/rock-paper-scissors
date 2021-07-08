c6f86fb4d229b3c74434d8126212d02193864bd5
Stephane Roy <stephroy411@hotmail.com>
2021-07-08 17:27:51 -0300

    Implemented javascript logic to play game

--- scripts/app.js
+++ scripts/app.js
@@ -1,22 +1,69 @@
+let humanScore = 0;
+let machineScore = 0;
+const humanScore_span = document.getElementById("human-score");
+const machineScore_span = document.getElementById("machine-score");
+const scoreBoard_div = document.querySelector(".score-board");
+const result_p = document.querySelector(".result > p");
+const rock_div = document.getElementById("Rock");
+const paper_div = document.getElementById("Paper");
+const scissors_div = document.getElementById("Scissors");
 
-let 
 
-function computerPlay() {
-    // random return (rock || paper || scissors)
+function getMachineChoice() {
+    const choices = ["Rock", "Paper", "Scissors"];
+    const randomNumber = Math.floor(Math.random() * 3);
+    return choices[randomNumber];
 }
 
-function playRound(playerSelection, computerSelection) {
-    if(playerSelection = winner)
-    return ("You win!");
-    else if(computerSelection = winner)
-    return ("you Lose!")
-    else
-    return ("It's a draw!")
+function win(humanChoice, machineChoice) {
+    humanScore++;
+    humanScore_span.innerHTML = humanScore;
+    machineScore_span.innerHTML = machineScore;
+    result_p.innerHTML = `${humanChoice} beats ${machineChoice}. You win!`;
 }
 
-function game() {
-    // Loop 10 times
-    function playRound() {
-        // Keep score, reports loser or winner
+function lose(humanChoice, machineChoice) {
+    machineScore++;
+    humanScore_span.innerHTML = humanScore;
+    machineScore_span.innerHTML = machineScore;
+    result_p.innerHTML = `${humanChoice} loses to ${machineChoice}.You lose!`;
+}
+
+function draw(humanChoice, machineChoice) {
+    result_p.innerHTML = `${humanChoice} equals ${machineChoice}.It\'s a draw!`;
+}
+
+function game(humanChoice) {
+    const machineChoice = getMachineChoice();
+    switch(humanChoice + machineChoice) {
+        case"RockScissors":
+        case "PaperRock":
+        case "ScissorsPaper":
+            win(humanChoice, machineChoice);
+            break;
+        case "ScissorsRock":
+        case "RockPaper":
+        case "PaperScissors":
+            lose(humanChoice, machineChoice);
+            break;
+        default:
+            draw(humanChoice, machineChoice);
+
     }
 }
+
+function main() {
+    rock_div.addEventListener("click", function() {
+        game("Rock")
+    })
+
+    paper_div.addEventListener("click", function() {
+        game("Paper")
+    })
+
+    scissors_div.addEventListener("click", function() {
+        game("Scissors")
+})
+}
+
+main();
